61367c051c214b95ac054ebab1fd498757d17a06
dplain90 <dplain@gmail.com>
2017-05-30 20:09:41 -0400

    Adjust height of display

--- lib/components/workstation/workstation.jsx
+++ lib/components/workstation/workstation.jsx
@@ -15,8 +15,8 @@ class WorkStation extends React.Component {
   componentDidMount() {
   let editorData = {
     x: 60,
-    width: 380,
-    height: 430
+    width: 480,
+    height: 600
   };
 
     this.stage = new createjs.Stage("workstationCanvas");
@@ -24,15 +24,12 @@ class WorkStation extends React.Component {
      this.stage.regX = this.stage.regY = -.5;
     this.palette = new Palette(this.stage, this.props.blockData);
     this.editor = new Editor(this.stage, editorData);
-    window.editor = this.editor;
-    window.palette = this.palette;
     this.stage.enableMouseOver(10);
     this.stage.mouseMoveOutside = true;
     this.stage.isMainStage = true;
     let cxt = this.stage.canvas.getContext("2d");
     cxt.webkitImageSmoothingEnabled = cxt.mozImageSmoothingEnabled = true;
 
-    window.stage = this.stage;
     this.stage.update();
     createjs.Ticker.addEventListener("tick", this.handleTick);
     this.props.updateCode(this.editor.code);
@@ -45,7 +42,7 @@ class WorkStation extends React.Component {
   render(){
     return (
       <div className="workstation">
-        <canvas id="workstationCanvas" width="500px" height="500px">
+        <canvas id="workstationCanvas" width="700px" height="700px">
         </canvas>
       </div>
     );
